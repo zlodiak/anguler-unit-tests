8c9541a0ca884908c373c1e95c26135364522f4b
kalinin <sergey.kalinin@simbirsoft.com>
2021-03-19 13:51:48 +0300

    simulate click ok

--- src/app/components/page3/page3.component.spec.ts
+++ src/app/components/page3/page3.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { Page3Component } from './page3.component';
 
@@ -21,5 +21,44 @@ describe('Page3Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be getWords-button', () => {
+    fixture.detectChanges();
+    const compliedComponent = fixture.debugElement.nativeElement;
+    expect(compliedComponent.querySelector('#getWordsBtn')).toBeTruthy();
+  });
+
+  it('should be getWordsFromComponet-button', () => {
+    fixture.detectChanges();
+    const compliedComponent = fixture.debugElement.nativeElement;
+    expect(compliedComponent.querySelector('#getWordsFromComponet')).toBeTruthy();
+  });
+
+  it('should display list after getWordsFromComponent-button', () => {
+    fixture.detectChanges();
+    const compliedComponent = fixture.debugElement.nativeElement;
+
+    const btn = compliedComponent.querySelector('#getWordsFromComponet');
+    btn.dispatchEvent(new Event('click'));
+
+    setTimeout(() => {
+      expect(compliedComponent.querySelector('.word-elem')).toBeTruthy();  
+    }, 100);
+  });
+
+  it('should display list after getWords-button', () => {
+    fixture.detectChanges();
+    const compliedComponent = fixture.debugElement.nativeElement;
+
+    const btn = compliedComponent.querySelector('#getWordsBtn');
+    btn.dispatchEvent(new Event('click'));
+
+    setTimeout(() => {
+      expect(compliedComponent.querySelector('.word-el')).toBeTruthy();  
+    }, 1000);
+
+    // tick(1000);
+    // expect(compliedComponent.querySelector('.word-el')).toBeTruthy(); 
   });
 });
